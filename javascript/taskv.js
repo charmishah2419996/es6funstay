c2a2083df84c96e46ff5a04b14bf1069da198f28
charmi shah <charmishah241996@gmail.com>
2019-07-29 13:40:43 +0545

    first commit

---
+++ javascript/taskv.js
@@ -0,0 +1,21 @@
+
+
+var x;
+function changeColor() {
+    x = document.getElementById("name").value;
+    console.log("hbjm ",x);
+    // if (x === "none") {
+    //   x.style.backgroundColor = "red";
+    // } else {
+    //   x.style.display = "none";
+    // }
+  }
+   
+
+
+
+
+
+
+
+
